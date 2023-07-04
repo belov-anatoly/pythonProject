286e339dc1ff639a3dd6d7153994804f0ff09d51
Student <liskotov@bk.ru>
2023-07-04 16:47:25 +0300

    JSON Loads and Parse

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import json
+import requests
+from datetime import datetime
 
-weekdays = ['Понедельник','Вторник','Среда','Четверг',
-'Пятница','Суббота', 'Воскресенье']
+town = input('Введите город: ')
+
+key = 'a55fd0196b4eb669cacc8ff27e977bda'
+url = 'http://api.openweathermap.org/data/2.5/weather'
+params = {'APPID': key, 'q': 'Санкт Петербург', 'units': 'metric'}
+
+request = requests.get(url, params=params)
+
+result = request.json()
+# print(result)
+code = result['cod']
 
 
-
-week_dict = {key: val for key, val in enumerate(weekdays)}
-
-data = json.dumps(week_dict)
-
-print(data)
+if code != '401':
+    if code != '404':
+        data = result['main']
+        print(f'Температура: {data["temp"]:.1f}\xB0C')
+        print('Влажность', data['humidity'], '%')
+        raw_time_sunset = result['sys']['sunset']
+        print(datetime.utcfromtimestamp(raw_time_sunset).strftime("%H:%M"))
+        print(f'Координаты: {result["coord"]["lon"]}, {result["coord"]["lat"]}')
+        if result['weather'][0]['main'] == 'Clouds':
+            print('Облачно')
+else:
+    print('Информации нет!')
