766720228edb1a08ddeedb91d9c0359e9995ab52
Student <liskotov@bk.ru>
2023-07-04 15:10:58 +0300

    JSON Dump to file

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
+import json  # Java Script Object Notation
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pets = {
+    'name': 'Charly',
+    'age': 15,
+    'meals': ['Purina', 'Hills'],
+    'owner': {'fname': 'Bill', 'sname': 'Gates'}
+}
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# 11111111111111111111111111111111111111111
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open('pets.json', 'w') as pet_file:
+    json.dump(pets, pet_file)
